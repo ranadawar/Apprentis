986750937fd7019099034543859dbbe6d2f339cc
Rana Dawar Abdullah <ranadawarabdullah@192.168.1.10>
2024-01-27 17:34:56 +0500

    Initialized Project Apprentis

---
+++ app/components/AppTextInput.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AppTextInput = () => {
+  return (
+    <View>
+      <Text>AppTextInput</Text>
+    </View>
+  );
+};
+
+export default AppTextInput;
+
+const styles = StyleSheet.create({});
